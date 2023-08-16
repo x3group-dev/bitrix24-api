2ae4fe01dda6c465aeacd7ecf1b4ebd9dfe239dc
Maxim Yugov <yugov.m@x3group.ru>
2023-08-16 17:58:05 +0300

    bizproc.event.send

---
+++ src/EntitiesServices/Bizproc/Event.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bitrix24Api\EntitiesServices\Bizproc;
+
+use Bitrix24Api\EntitiesServices\BaseEntity;
+use Bitrix24Api\Models\Bizproc\EventModel;
+
+class Event extends BaseEntity
+{
+    protected string $method = 'bizproc.event.%s';
+    public const ITEM_CLASS = EventModel::class;
+    protected string $resultKey = '';
+
+    public function send(array $params = []): bool
+    {
+        try {
+            $this->api->request(sprintf($this->getMethod(), 'send'), $params);
+            return true;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
