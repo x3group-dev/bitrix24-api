29f722890988bfc53d189d1034c106dc9a7ca813
Maxim Yugov <maxim.s.yugov@gmail.com>
2023-12-21 10:34:06 +0300

    del unused traits

--- src/EntitiesServices/Log/Blogpost.php
+++ src/EntitiesServices/Log/Blogpost.php
@@ -3,13 +3,6 @@
 namespace Bitrix24Api\EntitiesServices\Log;
 
 use Bitrix24Api\EntitiesServices\BaseEntity;
-use Bitrix24Api\EntitiesServices\Traits\Base\AddTrait;
-use Bitrix24Api\EntitiesServices\Traits\Base\DeleteTrait;
-use Bitrix24Api\EntitiesServices\Traits\Base\FieldsTrait;
-use Bitrix24Api\EntitiesServices\Traits\Base\GetListFastTrait;
-use Bitrix24Api\EntitiesServices\Traits\Base\GetListTrait;
-use Bitrix24Api\EntitiesServices\Traits\Base\GetTrait;
-use Bitrix24Api\EntitiesServices\Traits\Base\UpdateTrait;
 use Bitrix24Api\Exceptions\NotImplement;
 use Bitrix24Api\Models\Log\BlogpostModel;
 
